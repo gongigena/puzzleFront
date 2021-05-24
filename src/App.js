719bb642343cc8ae78681408100c8c658b8d3682
Gonzalo Gigena <gongigena@hotmail.com>
2021-05-24 18:57:12 -0300

    TimeInterval implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React , { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import './App.css';
 
-
-
+let timer = null;
 function App() {
   const [counter, setCounter] = useState(0);
 
-  const onStart = () => {
-    setCounter(counter + 1);
+  const OnStart = (setCounter,counter) => {
+    timer = setInterval(() => {
+      console.log(counter)
+      setCounter(preCounter => preCounter + 1);
+    }, 500);
   }
-
+  
   const onStop = () => {
-    console.log('stop');
+    clearInterval(timer);
   }
-
-  const onReset = () => {
+  
+  const onReset = (setCounter) => {
+    clearInterval(timer);
     setCounter(0);
   }
 
@@ -27,19 +30,21 @@ function App() {
           {counter}
         </p>
         <Grid
+          item
           container
           justify="center"
         >
           <Grid
+            item
             xs={3}
             container
             direction="row"
             justify="space-evenly"
             alignItems="center"
           >
-            <Button variant="contained" onClick={onStart}>Start</Button>
+            <Button variant="contained" onClick={() => OnStart(setCounter,counter)}>Start</Button>
             <Button variant="contained" onClick={onStop}>Stop</Button>
-            <Button variant="contained" onClick={onReset}>Reset </Button>
+            <Button variant="contained" onClick={() => { onReset(setCounter)}}>Reset </Button>
           </Grid>
         </Grid>
       </div>
